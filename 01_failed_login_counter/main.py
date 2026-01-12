be683b550f08a5f6370f1e08cd01b10020fdd233
Gonzalo Palomino <104773712+gpalomino91@users.noreply.github.com>
2026-01-12 08:08:36 -0600

    Update main.py

--- 01_failed_login_counter/main.py
+++ 01_failed_login_counter/main.py
@@ -1,21 +1,29 @@
 with open('sample.log', 'r',  encoding='utf-8') as file_log:
   d1 = {}
-  lines = []
-  user = ''
+  
   
   for line in file_log:
     if 'Login failed' in line:
       lines = line.split()
+      user = ''
+      ip = ''
+      key = ''
+      
       for word in lines:
-        user = ''
         if word.startswith('user=') and len(word) > 5:
           user = word[5:]
         elif word.startswith('ip=') and len(word) > 3:
-          user = word[3:]
-        else:
-          user = 'unknown'
-      if user not in d1:
-        d1[user] = 1
+          ip = word[3:]
+      if user != '':
+        key = user
+      elif ip != '':
+        key = ip
       else:
-        d1[user] += 1
-      
+        key = 'unknown'
+        
+      if key not in d1:
+        d1[key] = 1
+      else:
+        d1[key] += 1
+
+print()
