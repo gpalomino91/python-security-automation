524b71c1125496661dc0f1a1ebbe3eeffb320f41
Gonzalo Palomino <104773712+gpalomino91@users.noreply.github.com>
2026-01-12 13:55:00 -0600

    Implement login failure detection from log file
    Added logic to parse login failure messages from a log file and count occurrences by user or IP address.

--- 02_bruteforce_detector/main.py
+++ 02_bruteforce_detector/main.py
@@ -1,1 +1,35 @@
+with open("sample.log", "r") as file:
+  d1 = {}
+  
+  for line in file:
+    if 'Login failed' in line:
+      row = line.split()
+      user = ''
+      ip = ''
+      key = ''
 
+      for word in row:
+        if word.startswith('user=') and len(word) > 5:
+          user = word[5:]
+        elif word.starswith('ip=') and len(word) > 3:
+          ip = word[3:]
+          
+      if user != '':
+        key = user
+      elif ip != '':
+        key = ip
+      else:
+        key = 'unknown'
+
+      if key not in d1:
+        d1[key] = 1
+      else:
+        d1[key] += 1
+
+
+        
+        
+
+    
+    
+    
