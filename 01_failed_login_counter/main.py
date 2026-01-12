edf3ff339d7a93e3b3ff3848cc1c5494b85c765b
Gonzalo Palomino <104773712+gpalomino91@users.noreply.github.com>
2026-01-12 07:20:03 -0600

    Implement failed login counter from log file
    Count failed login attempts by user and IP from log.

--- 01_failed_login_counter/main.py
+++ 01_failed_login_counter/main.py
@@ -1,5 +1,21 @@
 with open('sample.log', 'r',  encoding='utf-8') as file_log:
-  dict = {}
+  d1 = {}
+  lines = []
+  user = ''
   
   for line in file_log:
-    if
+    if 'Login failed' in line:
+      lines = line.split()
+      for word in lines:
+        user = ''
+        if word.startswith('user=') and len(word) > 5:
+          user = word[5:]
+        elif word.startswith('ip=') and len(word) > 3:
+          user = word[3:]
+        else:
+          user = 'unknown'
+      if user not in d1:
+        d1[user] = 1
+      else:
+        d1[user] += 1
+      
