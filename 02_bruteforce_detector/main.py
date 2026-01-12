44e027e15a73eaaf1e461dc5252531f4cd4f79cf
Gonzalo Palomino <104773712+gpalomino91@users.noreply.github.com>
2026-01-12 14:08:15 -0600

    Correct typo and enhance no failed logins message
    Fix typo in 'startswith' method and improve output for no failed logins.

--- 02_bruteforce_detector/main.py
+++ 02_bruteforce_detector/main.py
@@ -11,7 +11,7 @@ with open("sample.log", "r") as file:
       for word in row:
         if word.startswith('user=') and len(word) > 5:
           user = word[5:]
-        elif word.starswith('ip=') and len(word) > 3:
+        elif word.startswith('ip=') and len(word) > 3:
           ip = word[3:]
           
       if user != '':
@@ -27,7 +27,14 @@ with open("sample.log", "r") as file:
         d1[key] += 1
 
 
-        
+  if not d1:
+    print('No failed logins found.')
+  else:
+    print('Bruteforce suspects (threshold=3):')
+    for key, value in d1.items():
+      if value >= 3 and key != 'unknown':
+        print(f'Identifier: {key} Fails: {value}')
+      
         
 
     
