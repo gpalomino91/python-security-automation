e2a759ac243444b51e2f562dae77a00bdb0f5383
Gonzalo Palomino <104773712+gpalomino91@users.noreply.github.com>
2026-01-16 08:45:36 -0600

    Update integrity_checker.py

--- 04_file_integrity_checker/integrity_checker.py
+++ 04_file_integrity_checker/integrity_checker.py
@@ -1,8 +1,39 @@
 import os
+import hashlib
 
 def init():
-  base_dir = 'watched'
+    base_dir = 'watched'
 
-  if not os.path.isdir(base_dir):
-    print('Directory not found') 
-    return
+    if not os.path.isdir(base_dir):
+        print('Directory not found')
+        return
+
+    baseline_data = {}
+
+    for current_dir, subdirs, files in os.walk(base_dir):
+
+        for file in files:
+            full_path = os.path.join(current_dir, file)
+            rel_path = os.path.relpath(full_path, base_dir)
+
+            size = os.path.getsize(full_path)
+            mtime = os.path.getmtime(full_path)
+            sha256 = sha256_file(full_path)
+
+
+            baseline_data[rel_path] = {
+                'size': size,
+                'mtime': mtime,
+                'SHA256': sha256
+            }
+
+def sha256_file(full_path, chunk_size=8192):
+  h = hashlib.sha256()
+  with open(full_path, "rb") as f:
+      while True:
+          chunk = f.read(chunk_size)
+          if not chunk:
+              break
+          h.update(chunk)
+  return h.hexdigest()
+
